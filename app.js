e0dc4c21cba5aab09f1efb1207f89a360a032e53
Sean Huggins <53221773+MadeWithHuggs@users.noreply.github.com>
2024-11-14 19:41:02 -0500

    Initial Commit
    Initial commit with beginning files.

---
+++ app.js
@@ -0,0 +1,89 @@
+function solution1Function() {
+  var x = document.getElementById("Solution1");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function solution2Function() {
+  var x = document.getElementById("Solution2");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function solution3Function() {
+  var x = document.getElementById("Solution3");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function solution4Function() {
+  var x = document.getElementById("Solution4");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function solution5Function() {
+  var x = document.getElementById("Solution5");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function solution6Function() {
+  var x = document.getElementById("Solution6");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function solution7Function() {
+  var x = document.getElementById("Solution7");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function solution8Function() {
+  var x = document.getElementById("Solution8");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function solution9Function() {
+  var x = document.getElementById("Solution9");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function solution10Function() {
+  var x = document.getElementById("Solution10");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
